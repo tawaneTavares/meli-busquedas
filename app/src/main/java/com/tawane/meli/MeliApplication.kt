dfc6c1c41c7e7e367e5d6cbc4e94d5713dd681d6
tawaneTavares <tawane_cristina@hotmail.com>
2025-03-31 20:14:47 -0300

    :sparkles: add timber config

--- app/src/main/java/com/tawane/meli/MeliApplication.kt
+++ app/src/main/java/com/tawane/meli/MeliApplication.kt
@@ -1,8 +1,16 @@
 package com.tawane.meli
 
 import android.app.Application
+import com.tawane.data.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MeliApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
