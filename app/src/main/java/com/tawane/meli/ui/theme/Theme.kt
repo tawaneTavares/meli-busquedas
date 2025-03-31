bfc07185148be4bb806062d804d92754841246ec
tawaneTavares <tawane_cristina@hotmail.com>
2025-03-31 19:44:31 -0300

    :art: remove unused import

--- app/src/main/java/com/tawane/meli/ui/theme/Theme.kt
+++ app/src/main/java/com/tawane/meli/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.tawane.meli.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
