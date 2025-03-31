6508a1339a843d6f0b1805d39d4cbe86b785f97b
tawaneTavares <tawane_cristina@hotmail.com>
2025-03-31 20:14:15 -0300

    :art: ktlint corrections

--- app/src/main/java/com/tawane/meli/ui/MainActivity.kt
+++ app/src/main/java/com/tawane/meli/ui/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
