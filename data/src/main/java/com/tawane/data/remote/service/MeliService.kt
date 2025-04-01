80faf1976516b755dbab0a7699640992af38edef
tawaneTavares <tawane_cristina@hotmail.com>
2025-03-31 22:38:24 -0300

    :sparkles: create search api call

--- data/src/main/java/com/tawane/data/remote/service/MeliService.kt
+++ data/src/main/java/com/tawane/data/remote/service/MeliService.kt
@@ -1,3 +1,15 @@
 package com.tawane.data.remote.service
 
-interface MeliService
+import com.tawane.data.remote.model.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MeliService {
+
+    @GET("sites/{site_id}/search/{q}")
+    suspend fun searchItems(
+        @Path("site_id") siteId: String,
+        @Path("q") query: String,
+        @Path("page_position") position: Int,
+    ): ApiResponse
+}
